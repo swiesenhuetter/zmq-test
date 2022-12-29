cee3751b147ff02c19122702eb8a0499b3613012
Stephan Wiesenhütter <wiesenhuetter@gmail.com>
2022-12-29 20:48:17 +0100

    talks to python

--- Source.cpp
+++ Source.cpp
@@ -25,7 +25,7 @@ int main()
 				std::this_thread::sleep_for(std::chrono::seconds(1));
 				auto received = receiver.recv(msg, zmq::recv_flags::none);
 				auto pers = msgpack::unpack<TestRecord>(static_cast<uint8_t*>(msg.data()), msg.size());
-				std::wcout << "Received " << pers << " bytes\n";
+				std::wcout << "Received : " << pers << "\n";
 
 			}
 			receiver.send(zmq::buffer("Hello from thread 1"), zmq::send_flags::none);
@@ -34,11 +34,13 @@ int main()
 
 	zmq::socket_t sock(ctx, zmq::socket_type::pair);
 	sock.bind("inproc://test");
-
+	sock.connect("tcp://localhost:5555");
+	
 	TestRecord person;
 	auto sbuf = msgpack::pack(person);
 	
 	sock.send(zmq::message_t(sbuf), zmq::send_flags::none);
+	
 	std::this_thread::sleep_for(std::chrono::seconds(2));
 	sock.send(zmq::message_t(sbuf), zmq::send_flags::none);
 	stoken = true;
@@ -46,16 +48,16 @@ int main()
 	std::this_thread::sleep_for(std::chrono::seconds(2));
 	auto msg = zmq::message_t();
 	auto received = sock.recv(msg, zmq::recv_flags::none);
-	std::cout << "Received " << msg.to_string() << " bytes\n";
+	std::cout << "Received " << msg.to_string() << "\n";
 
 	received = sock.recv(msg, zmq::recv_flags::dontwait);
 	if (received.has_value()) {
-		std::cout << "Received " << msg.to_string() << " bytes\n";
+		std::cout << "Received " << msg.to_string() << "\n";
 	}
 	else {
 		std::cout << "No message received\n";
 	}
-	std::cout << "Received " << msg.to_string() << " bytes\n";
+	std::cout << "Received " << msg.to_string() << "\n";
 	received = sock.recv(msg, zmq::recv_flags::dontwait);
 
 }
---
+++ client.py
@@ -0,0 +1,20 @@
+import zmq
+import msgpack
+import io
+
+
+context = zmq.Context()
+print("Connecting to server...")
+socket = context.socket(zmq.PAIR)
+socket.bind("tcp://*:5555")
+print("Bind to 5555")
+
+for request in range(10):
+    print("recv...")
+    message = socket.recv()
+    unpack_data = io.BytesIO(message)
+    unpacker = msgpack.Unpacker(unpack_data, raw=False)
+    for unpacked in unpacker:
+        print(unpacked)    
+    
+    print(f"Received: {message}")
